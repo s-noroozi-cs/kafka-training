f9d87132d15aaaa7f92395817e708f24d206e324
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-08-11 12:13:42 +0430

    improve ordering tests

--- sample-projects/src/test/java/OrderingTests.java
+++ sample-projects/src/test/java/OrderingTests.java
@@ -1,7 +1,11 @@
+import com.kafka.config.FixedPartitioner;
+import lombok.NoArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.Cluster;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -77,6 +81,36 @@ public class OrderingTests {
         KafkaProducer producer = new KafkaProducer(KafkaUtil.getDefaultProducerConfig());
         IntStream.range(0, 10)
                 .mapToObj(i -> new ProducerRecord(topicName,"keep-order",String.valueOf(i)))
+                .forEach(producer::send);
+        producer.close();
+
+        Map<String, String> cfg = KafkaUtil.getDefaultConsumerConfig();
+        cfg.put(KafkaUtil.KAFKA_CONFIG_AUTO_OFFSET_RESET, KafkaUtil.OFFSET_RESET_EARLIEST);
+        List<Integer> items = new ArrayList();
+        KafkaConsumer consumer = new KafkaConsumer(cfg);
+        consumer.subscribe(Arrays.asList(topicName));
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(300));
+        records.forEach(i -> items.add(Integer.parseInt(i.value())));
+        consumer.unsubscribe();
+        consumer.close();
+
+        for (int i = 0; i < 10; i++) {
+            Assertions.assertEquals(i, items.get(i));
+        }
+    }
+
+    @Test
+    void test_order_with_custom_biz(){
+        String topicName = Util.getRandomTopicName();
+        int partitions = 10;
+        KafkaUtil.createTopic(topicName,partitions);
+
+        Map producerConfig = KafkaUtil.getDefaultProducerConfig();
+        producerConfig.put(KafkaUtil.KAFKA_PARTITIONER_CLASS, FixedPartitioner.class.getName());
+
+        KafkaProducer producer = new KafkaProducer(producerConfig);
+        IntStream.range(0, 10)
+                .mapToObj(i -> new ProducerRecord(topicName,String.valueOf(i),String.valueOf(i)))
                 .forEach(producer::send);
         producer.close();
 
