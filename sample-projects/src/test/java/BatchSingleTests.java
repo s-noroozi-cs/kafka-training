a780e3bfb233eca31bb30ccb35a60fe60ca192a4
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-08-13 22:18:09 +0430

    Update BatchSingleTests.java

--- sample-projects/src/test/java/BatchSingleTests.java
+++ sample-projects/src/test/java/BatchSingleTests.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 public class BatchSingleTests {
+
+    @Test
+    void test_single_mode_produce(){
+        String topicName = Util.getRandomTopicName();
+    }
 }
