e5811a59f4b4bf5ab8717d1380f5a042fa52041f
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-08-20 20:43:40 +0430

    Update TransactionalTests.java

--- sample-projects/src/test/java/TransactionalTests.java
+++ sample-projects/src/test/java/TransactionalTests.java
@@ -1,2 +1,62 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.IsolationLevel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
 public class TransactionalTests {
+    @Test
+    void check_isolation_level_read_uncommitted() {
+        String topic = Util.getRandomTopicName();
+        KafkaProducer producer = new KafkaProducer(KafkaUtil.getDefaultProducerConfig(
+                ProducerConfig.TRANSACTIONAL_ID_CONFIG, Util.getRandomProducerTrxCfg()));
+
+        KafkaConsumer consumer = KafkaUtil.getConsumer(topic,
+                KafkaUtil.KAFKA_CONFIG_AUTO_OFFSET_RESET, KafkaUtil.OFFSET_RESET_EARLIEST);
+
+        producer.initTransactions();
+        producer.beginTransaction();
+        producer.send(new ProducerRecord(topic, "A"));
+
+        ConsumerRecords records = consumer.poll(Duration.ofMillis(400));
+        Assertions.assertEquals(1, records.count());
+
+        producer.abortTransaction();
+    }
+
+    @Test
+    void check_isolation_level_read_committed() {
+        String topic = Util.getRandomTopicName();
+        KafkaProducer producer = new KafkaProducer(KafkaUtil.getDefaultProducerConfig(
+                ProducerConfig.TRANSACTIONAL_ID_CONFIG, Util.getRandomProducerTrxCfg()));
+
+        KafkaConsumer consumer = KafkaUtil.getConsumer(topic,
+                KafkaUtil.KAFKA_CONFIG_AUTO_OFFSET_RESET, KafkaUtil.OFFSET_RESET_EARLIEST,
+                ConsumerConfig.ISOLATION_LEVEL_CONFIG, IsolationLevel.READ_COMMITTED.name().toLowerCase()
+        );
+
+        producer.initTransactions();
+        producer.beginTransaction();
+        producer.send(new ProducerRecord(topic, "A"));
+
+        ConsumerRecords records = consumer.poll(Duration.ofMillis(400));
+        Assertions.assertEquals(0, records.count());
+        producer.commitTransaction();
+
+        producer.beginTransaction();
+        producer.send(new ProducerRecord(topic, "A"));
+        producer.commitTransaction();
+
+        records = consumer.poll(Duration.ofMillis(400));
+        Assertions.assertEquals(1, records.count());
+
+
+
+    }
 }
