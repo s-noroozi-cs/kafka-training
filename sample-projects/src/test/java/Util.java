c3b6c6b98a790f6229d9dd95d7728841d3cd8f4d
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-08-20 20:28:02 +0430

    Update Util.java

--- sample-projects/src/test/java/Util.java
+++ sample-projects/src/test/java/Util.java
@@ -2,15 +2,19 @@ import java.util.Random;
 
 public class Util {
 
-    public static int getRandomNumber() {
-        return new Random().nextInt(10_000);
+    public static int getRandomNumber(int bound) {
+        return new Random().nextInt(10 * bound);
     }
 
     public static String getRandomTopicName() {
-        return "test-" + getRandomNumber();
+        return "test-" + getRandomNumber(4);
+    }
+
+    public static String getRandomProducerTrxCfg(){
+        return "prod-" + getRandomNumber(1);
     }
 
     public static String getRandomConsumerGroupId() {
-        return "test-app-consumer-" + getRandomNumber();
+        return "test-app-consumer-" + getRandomNumber(4);
     }
 }
