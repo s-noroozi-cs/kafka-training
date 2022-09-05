513473e6ca06c3b0fdaa480e1bbacca782d2240b
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-09-05 22:37:57 +0430

    Update ZookeeperTests.java

--- sample-projects/src/test/java/ZookeeperTests.java
+++ sample-projects/src/test/java/ZookeeperTests.java
@@ -1,24 +1,28 @@
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ZookeeperTests {
 
     private ZooKeeper zk;
-
+    
     @BeforeEach
     void connect_zookeeper() {
         try {
+            CountDownLatch connectedSignal = new CountDownLatch(1);
             zk = new ZooKeeper("localhost", 30000, new Watcher() {
                 @Override
-                public void process(WatchedEvent watchedEvent) {
-
+                public void process(WatchedEvent we) {
+                    if (we.getState() == Event.KeeperState.SyncConnected) {
+                        connectedSignal.countDown();
+                    }
                 }
             });
+            connectedSignal.await();
         } catch (Throwable ex) {
             throw new RuntimeException(ex.getMessage(), ex);
         }
@@ -31,5 +35,12 @@ public class ZookeeperTests {
         } catch (Throwable ex) {
             throw new RuntimeException(ex.getMessage(), ex);
         }
+    }
+
+    @Test
+    void check_zookeeper_connection() {
+        Assertions.assertNotNull(zk);
+        Assertions.assertEquals(true, zk.getState().isConnected());
+        Assertions.assertEquals(true, zk.getState().isAlive());
     }
 }
