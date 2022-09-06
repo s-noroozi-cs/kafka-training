28cfeb31a55090e7458a2c69b0b84d02fa6149c8
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-09-06 23:11:34 +0430

    Update ZookeeperTests.java

--- sample-projects/src/test/java/ZookeeperTests.java
+++ sample-projects/src/test/java/ZookeeperTests.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
+import java.time.temporal.ChronoField;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class ZookeeperTests {
 
@@ -74,6 +76,36 @@ public class ZookeeperTests {
             });
 
             zk.setData(path, "new-data".getBytes(StandardCharsets.UTF_8), 1);
+
+        } catch (Throwable ex) {
+            throw new RuntimeException(ex.getMessage(), ex);
+        }
+    }
+
+    @Test
+    void check_watcher() {
+        try {
+            String path = "/" + UUID.randomUUID();
+            CountDownLatch nodeWatcher = new CountDownLatch(2);
+            zk.addWatch(path, new Watcher() {
+                @Override
+                public void process(WatchedEvent we) {
+                    if(Event.EventType.NodeCreated.equals(we.getType())
+                            || Event.EventType.NodeDataChanged.equals(we.getType())){
+                        if(path.equals(we.getPath())){
+                            nodeWatcher.countDown();
+                        }
+                    }
+
+                }
+            },AddWatchMode.PERSISTENT);
+
+            zk.create(path, "data".getBytes(StandardCharsets.UTF_8), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zk.setData(path, "new-data".getBytes(StandardCharsets.UTF_8), 0);
+
+            nodeWatcher.await(3000, TimeUnit.MILLISECONDS);
+
+            Assertions.assertEquals(0L,nodeWatcher.getCount());
 
         } catch (Throwable ex) {
             throw new RuntimeException(ex.getMessage(), ex);
