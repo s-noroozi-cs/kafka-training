5bded4fc1e38988bd99fb4c7e0a022dcc373e282
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-09-12 22:22:18 +0430

    Update ConsumeCustomMessageTests.java

--- sample-projects/src/test/java/ConsumeCustomMessageTests.java
+++ sample-projects/src/test/java/ConsumeCustomMessageTests.java
@@ -4,38 +4,32 @@ import org.apache.kafka.clients.consumer.OffsetAndTimestamp;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.TopicPartition;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ConsumeCustomMessageTests {
 
-    private void consume_records(KafkaConsumer consumer, long epochSeconds) {
+    private ConsumerRecords consume_records(KafkaConsumer consumer, long timestamp) {
         Map<TopicPartition, Long> partitionTimestampMap =
                 (Map<TopicPartition, Long>) consumer.assignment()
                         .stream()
-                        .collect(Collectors.toMap(tp -> tp, tp -> epochSeconds));
+                        .collect(Collectors.toMap(tp -> tp, tp -> timestamp));
         Map<TopicPartition, OffsetAndTimestamp> offsetMap
                 = consumer.offsetsForTimes(partitionTimestampMap);
         for (Map.Entry<TopicPartition, OffsetAndTimestamp> entry : offsetMap.entrySet()) {
             consumer.seek(entry.getKey(), entry.getValue().offset());
         }
+        return consumer.poll(Duration.ofMillis(500));
     }
 
     @Test
     void consume_specific_offset() {
-
-        //        Long oneHourEarlier = Instant.now().atZone(ZoneId.systemDefault())
-//                .minusHours(1).toEpochSecond();
-
         String topic = Util.getRandomTopicName();
 
         KafkaProducer producer = new KafkaProducer(KafkaUtil.getDefaultProducerConfig());
@@ -43,16 +37,25 @@ public class ConsumeCustomMessageTests {
                 .mapToObj(String::valueOf)
                 .map(msg -> new ProducerRecord(topic, msg))
                 .forEach(item -> {
-                    Thread.sleep(1500);
+                    Util.sleep(1000);
+                    producer.send(item);
                 });
         producer.close();
+
+        long timestamp = System.currentTimeMillis()-5000;
 
         Map<String, String> cfg = KafkaUtil.getDefaultConsumerConfig();
         cfg.put(KafkaUtil.KAFKA_CONFIG_AUTO_OFFSET_RESET, KafkaUtil.OFFSET_RESET_EARLIEST);
 
         KafkaConsumer consumer = new KafkaConsumer(cfg);
         consumer.subscribe(Arrays.asList(topic));
-        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(300));
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1500));
+        Assertions.assertEquals(10, records.count());
+
+
+
+        records = consume_records(consumer,timestamp);
+        Assertions.assertEquals(5, records.count());
 
     }
 }
