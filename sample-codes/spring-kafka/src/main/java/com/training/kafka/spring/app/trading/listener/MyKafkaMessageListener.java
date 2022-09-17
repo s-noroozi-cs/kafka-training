f55ec415565021ad35d1927c3a7983074e38538f
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-09-17 19:10:13 +0430

    Create MyKafkaMessageListener.java

---
+++ sample-codes/spring-kafka/src/main/java/com/training/kafka/spring/app/trading/listener/MyKafkaMessageListener.java
@@ -0,0 +1,17 @@
+package com.training.kafka.spring.app.trading.listener;
+
+import org.apache.kafka.common.protocol.Message;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+public class MyKafkaMessageListener {
+
+
+    @KafkaListener(id="${kafka.consumer.id:myKafkaMsgConsumer}"
+            , autoStartup = "false"
+            , topics = "${kafka.consumer.topic:test}")
+    public void listen(Message message){
+
+    }
+
+}
