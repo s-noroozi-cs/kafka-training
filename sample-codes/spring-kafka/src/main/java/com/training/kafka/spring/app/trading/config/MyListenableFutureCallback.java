9e5e2037807d87fd7c0fba2c501c594a32a674d7
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-09-17 17:23:26 +0430

    Update MyListenableFutureCallback.java

--- sample-codes/spring-kafka/src/main/java/com/training/kafka/spring/app/trading/config/MyListenableFutureCallback.java
+++ sample-codes/spring-kafka/src/main/java/com/training/kafka/spring/app/trading/config/MyListenableFutureCallback.java
@@ -1,15 +1,16 @@
 package com.training.kafka.spring.app.trading.config;
 
+import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
-public class MyListenableFutureCallback implements ListenableFutureCallback {
+public class MyListenableFutureCallback implements ListenableFutureCallback<SendResult<String, String>> {
     @Override
     public void onFailure(Throwable ex) {
         
     }
 
     @Override
-    public void onSuccess(Object result) {
+    public void onSuccess(SendResult<String, String> result) {
 
     }
 }
