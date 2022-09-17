1923d35e12a67558824ac1c9fb3aca2e79216bb8
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-09-17 17:18:23 +0430

    Update KafkaController.java

--- sample-codes/spring-kafka/src/main/java/com/training/kafka/spring/app/trading/controller/KafkaController.java
+++ sample-codes/spring-kafka/src/main/java/com/training/kafka/spring/app/trading/controller/KafkaController.java
@@ -1,10 +1,21 @@
 package com.training.kafka.spring.app.trading.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/api/v1/controller")
 public class KafkaController {
-    
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("/messages/{topic}")
+    public ResponseEntity sendSingleMessage(@PathVariable("topic") String topic
+            ,@RequestBody String message){
+        kafkaTemplate.send(topic,message);
+        return ResponseEntity.ok().build();
+    }
 }
