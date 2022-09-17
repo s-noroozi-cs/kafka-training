c6817e4564a877f6190594a2ddb3e398bc883804
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-09-17 17:22:21 +0430

    Update KafkaController.java

--- sample-codes/spring-kafka/src/main/java/com/training/kafka/spring/app/trading/controller/KafkaController.java
+++ sample-codes/spring-kafka/src/main/java/com/training/kafka/spring/app/trading/controller/KafkaController.java
@@ -1,8 +1,11 @@
 package com.training.kafka.spring.app.trading.controller;
 
+import com.training.kafka.spring.app.trading.config.MyListenableFutureCallback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
@@ -13,9 +16,9 @@ public class KafkaController {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     @PostMapping("/messages/{topic}")
-    public ResponseEntity sendSingleMessage(@PathVariable("topic") String topic
-            ,@RequestBody String message){
-        kafkaTemplate.send(topic,message);
+    public ResponseEntity sendSingleMessage(@PathVariable("topic") String topic, @RequestBody String message) {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, message);
+        future.addCallback(new MyListenableFutureCallback());
         return ResponseEntity.ok().build();
     }
 }
